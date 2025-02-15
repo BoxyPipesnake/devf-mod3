92f1ed01a85a16dd6e862820c3d31c9dc756f905
BoxyPipesnake <felipe_motoss@hotmail.com>
2025-02-14 19:36:55 -0600

    Modifica ejercicio seguimiento libros añadiendo interaccion con usuario

--- seguimientoLibros/index.js
+++ seguimientoLibros/index.js
@@ -1,20 +1,38 @@
 // Define una función `agregarLibro(titulo)`, que añada un libro a un array llamado `librosLeidos`.
 // Define una función `mostrarLibrosLeidos()`, que imprima todos los libros que has leído.
 
+const nombreLibro = document.getElementById('nombre-libro');
+const listaLibros = document.getElementById("lista-libros");
+
+const agregarLibroBtn = document.getElementById('agrega-libro');
+const muestraLibrosBtn = document.getElementById('muestra-libros');
+
 const librosLeidos = [];
 
 function agregarLibro(titulo){
     librosLeidos.push(titulo);
 }
 
-function mostrarLibrosLeidos(){
-    for(let i = 1; i <= librosLeidos.length; i++){
-        console.log(i, librosLeidos[i - 1]);
+function mostrarLibrosLeidos() {
+    listaLibros.innerHTML = "";
+
+    for (let i = 0; i < librosLeidos.length; i++) { 
+        const li = document.createElement("li");
+        li.textContent = `${librosLeidos[i]}`;
+        listaLibros.appendChild(li);
     }
 }
 
-agregarLibro("El principito");
-agregarLibro("Sherlock Holmes");
-agregarLibro("Habitos Atomicos");
 
-mostrarLibrosLeidos();
+agregarLibroBtn.addEventListener('click', () => {
+    const titulo = nombreLibro.value.trim(); 
+    if (titulo) { // Verificar que no esté vacío
+        agregarLibro(titulo);
+        nombreLibro.value = "";
+    } else {
+        alert("Por favor, ingresa un título válido.");
+    }
+});
+
+
+muestraLibrosBtn.addEventListener('click', mostrarLibrosLeidos);
